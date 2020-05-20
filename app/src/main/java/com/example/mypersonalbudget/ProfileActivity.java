8231a95e0db700cd61d73be12c3757b897283196
Lisa <lisaburini.98@gmail.com>
2020-05-20 21:41:30 +0200

    Implementazione pagina profilo per visualizzare i dati dell'utente

--- app/src/main/java/com/example/mypersonalbudget/ProfileActivity.java
+++ app/src/main/java/com/example/mypersonalbudget/ProfileActivity.java
@@ -1,16 +1,29 @@
 package com.example.mypersonalbudget;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class ProfileActivity extends AppCompatActivity {
     private TextView firstName, lastName, email;
+    private static final String TAG = "ProfileActivity";
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +33,22 @@ public class ProfileActivity extends AppCompatActivity {
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        firstName = (TextView) findViewById(R.id.text_name);
+        firstName = (TextView)findViewById(R.id.text_name);
         lastName = (TextView) findViewById(R.id.text_last_name);
         email = (TextView) findViewById(R.id.text_email);
+
+        docRef = db.collection("utenti").document(uid);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String data_name = documentSnapshot.getString("nome");
+                String data_lName = documentSnapshot.getString("cognome");
+                String data_email = documentSnapshot.getString("email");
+                firstName.setText(data_name);
+                lastName.setText(data_lName);
+                email.setText(data_email);
+            }
+        });
     }
 
     @Override
