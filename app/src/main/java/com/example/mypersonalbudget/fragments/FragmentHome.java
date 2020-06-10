ab13e4e03d29847b570ad383e16e429d8e9406ec
Simone <S1081623@studenti.univpm.it>
2020-06-10 17:42:55 +0200

    Implementazione del fragment della Home

--- app/src/main/java/com/example/mypersonalbudget/fragments/FragmentHome.java
+++ app/src/main/java/com/example/mypersonalbudget/fragments/FragmentHome.java
@@ -1,6 +1,47 @@
 package com.example.mypersonalbudget.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.mypersonalbudget.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class FragmentHome extends Fragment {
+    private TextView welcome;
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        welcome = view.findViewById(R.id.text_welcome);
+
+        docRef = db.collection("utenti").document(uid);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String data_name = documentSnapshot.getString("nome");
+                String data_lName = documentSnapshot.getString("cognome");
+                welcome.setText("Welcome " + data_name + " " + data_lName + "!");
+            }
+        });
+
+        return view;
+    }
 }
