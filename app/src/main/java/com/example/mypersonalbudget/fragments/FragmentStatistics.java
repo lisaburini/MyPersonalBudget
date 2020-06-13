6a417446de1a43a7f0970494abacbcb8589a50e4
Michelangelo <michelangeloam98@gmail.com>
2020-06-13 18:49:52 +0200

    Inizio creazione grafici in sezione "Statistiche".

--- app/src/main/java/com/example/mypersonalbudget/fragments/FragmentStatistics.java
+++ app/src/main/java/com/example/mypersonalbudget/fragments/FragmentStatistics.java
@@ -1,6 +1,35 @@
 package com.example.mypersonalbudget.fragments;
 
+import android.os.Bundle;
+import android.view.MotionEvent;
+
 import androidx.fragment.app.Fragment;
 
-public class FragmentStatistics extends Fragment {
+import com.example.mypersonalbudget.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+public class FragmentStatistics extends Fragment implements OnChartGestureListener, OnChartValueSelectedListener {
+
+    private static final String TAG = "FragmentStatistics";
+
+    private LineChart GraficoBarre;
+
+    @Override
+    public void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_statistics);
+
+        GraficoBarre = (LineChart) findViewById(R.id.GrafBarre);
+
+        GraficoBarre.setOnChartListener(FragmentStatistics.this);
+        GraficoBarre.setOnClickListener
+    }
+
+
+
 }
