fce484cd3855c540e0a337aedc1b811f035aaf92
Simone <S1081623@studenti.univpm.it>
2020-06-27 21:29:09 +0200

    Sistemato il problema degli istogrammi sostituendoli con un grafico a torta

--- app/src/main/java/com/example/mypersonalbudget/fragments/FragmentStatistics.java
+++ app/src/main/java/com/example/mypersonalbudget/fragments/FragmentStatistics.java
@@ -13,10 +13,7 @@ import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Cartesian;
-import com.anychart.core.cartesian.series.Column;
-import com.anychart.enums.Anchor;
-import com.anychart.enums.Position;
+import com.anychart.charts.Pie;
 import com.example.mypersonalbudget.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,24 +30,34 @@ public class FragmentStatistics extends Fragment {
 
     String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    AnyChartView anyChartView;
+    Pie pie;
 
     @Override
     public void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        anyChartView = view.findViewById(R.id.graficoABarre);
+        pie = AnyChart.pie();
+        return view;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
 
         Query statisticsEarningsQuery = db.collection("utenti").document(uid).collection("transazioni");
         statisticsEarningsQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()) {
-
-                    // Crea il grafico a barre.
-                    Cartesian cartesian = AnyChart.column();
 
                     float sumEarnings = 0;
                     float sumOutflows = 0;
@@ -61,47 +68,26 @@ public class FragmentStatistics extends Fragment {
                         if(category.equals("Earnings")) {
                             sumEarnings += amount;
                         } else if(category.equals("Outflows")) {
-                            sumOutflows -= amount;
+                            sumOutflows += amount;
                         }
                     }
 
-                    /* Crea un Array list con tutti i valore da assegnare. Puoi teoricamente aggiungere
-                     * altre variabili come la "y" così da avere altri dati da inserire nella y.
-                     * Ricordati che per ogni variabile devi anc he dare un valore.
-                     *
-                     * Qui poi va anche tutto vuoto, perché i dati saranno presi ed iniettati da Firebase,
-                     * quelli che vedi ora sono stati inseriti manualmente da me per provare a vedere se
-                     * funzionasse...
-                     */
+                    float EarningsRound = (float) (Math.round(sumEarnings * 100.0) / 100.0);
+                    float OutflowsRound = (float) (Math.round(sumOutflows * 100.0) / 100.0);
+
                     List<DataEntry> data = new ArrayList<>();
-                    data.add(new ValueDataEntry("Earnings", sumEarnings));
-                    data.add(new ValueDataEntry("Outflows", sumOutflows));
+                    data.add(new ValueDataEntry("Earnings", EarningsRound));
+                    data.add(new ValueDataEntry("Outflows", OutflowsRound));
 
-                    Column column = cartesian.column(data);
+                    pie.data(data);
 
-                    column.tooltip()
-                            .titleFormat("{%X}")
-                            .position(Position.CENTER_BOTTOM)
-                            .anchor(Anchor.CENTER_BOTTOM)
-                            .offsetX(0d)
-                            .offsetY(5d)
-                            .format("${%Value}{groupsSeparator: }");
-
-                    cartesian.animation(true);
-
-                    cartesian.title("Wallet Graphic for the last Period");
-
-                    AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.graficoABarre);
-
-                    anyChartView.setChart(cartesian);
-
+                    anyChartView.setChart(pie);
                 } else {
                     Toast.makeText(getActivity().getApplicationContext(), getString(R.string.wrong),Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-    return view;
     }
 
 }
